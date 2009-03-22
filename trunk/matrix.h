6065789292f577050d5a4a4776651a0455500504
ryanlei750328 <ryanlei750328@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2009-03-22 16:43:00 +0000

    add matrix.h, matrix.c: the matrix implentations

---
+++ trunk/matrix.h
@@ -0,0 +1,45 @@
+/** File: matrix.h
+ ** Author: ryanlei
+ ** Creation : 2009/03/21
+ ** Modification: 2009/03/22
+ ** Description: matrix data structure 
+ **/
+typedef struct Matrix {
+	
+	/* array of values, 先一律用double 3D array */
+	double ***data; /* [ layer ][ row ][ col ] */
+	/* dimensions */
+	int size1; /* row: 碰到row vector的話size1 = 1 */
+	int size2; /* col: 碰到column vector的話size2 = 1 */
+	int size3; /* layer: 碰到2D的話size3 = 1 */
+
+} Matrix;
+
+/* matrix basics */
+typedef enum COLOR { R, G, B, ALL } COLOR; // 對於2D矩陣來說，R和ALL意義是一樣的
+typedef enum TYPE { INT, DOUBLE } TYPE;
+void dump( Matrix *source, char *name, COLOR color, TYPE type ); 
+// 顯示矩陣內容( 矩陣、名稱、第三維、INT或DOUBLE )
+void zeros( Matrix *source, int size1, int size2, int size3 ); // 產生全0的矩陣
+void ones( Matrix *source, int size1, int size2, int size3 ); // 產生全1的矩陣
+void eye( Matrix *source, int size ); // 產生2D Identity矩陣
+void RAND( Matrix *source, int size1, int size2, int size3, int lower, int upper ); 
+// 產生整數亂數[lower,upper]矩陣
+void freeMatrix( Matrix *source ); // 清除矩陣的記憶體空間
+
+/* scalar operations */
+void s_add( Matrix *source, double number ); // 元素加上一個純量
+void s_mul( Matrix *source, double number ); // 元素乘上一個純量（係數積）
+void s_pow( Matrix *source, double number ); // 元素的純量次方 
+
+/* matrix or element-wise operations */
+void m_add( Matrix *source1, Matrix *source2, Matrix *dest ); 
+// 元素對應相加，sources不可和dest相同
+void e_mul( Matrix *source1, Matrix *source2, Matrix *dest ); 
+// 元素對應相乘，sources不可和dest相同
+void m_mul( Matrix *source1, Matrix *source2, COLOR color, Matrix *dest ); 
+// 2D矩陣相乘，sources不可和dest相同
+
+/* to be added in util.h */
+void error( char *msg ); // 錯誤訊息，並中斷程式
+
