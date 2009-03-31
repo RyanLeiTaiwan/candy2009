ad37b58c43a053586190c4f249c80bd300edd001
ryanlei750328 <ryanlei750328@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2009-03-31 16:15:54 +0000

    a lot of changes to fix defects. = = 加油!

---
+++ image.c
@@ -0,0 +1,90 @@
+#include "image.h"
+#include "util.h"
+
+#define DEBUG 1
+
+
+void imread( char *filename, Matrix *dest ){  // dest is not yet allocted
+    FILE          *fp_s = NULL;  // source file handler
+    BMP_file_header file_header;
+    BMP_info_header info_header;
+    uint8 *image_s = NULL; // temp one-dimension array to save
+    int col,row,layer,ine;
+    //dest=NULL;
+
+    fp_s = fopen(filename, "rb");
+    if (fp_s == NULL) {
+        /* 請愛用自己寫的error: stderr兼跳出程式 */
+        error( "imread(): FIle open error." );
+    }
+    fseek(fp_s, 2, SEEK_SET);
+    fread(&file_header.file_size, sizeof(uint32), 1, fp_s);
+    fseek(fp_s, 10, SEEK_SET);
+    fread(&file_header.data_offset, sizeof(uint32), 1, fp_s);
+    fseek(fp_s, 18, SEEK_SET);
+    fread(&info_header.width, sizeof(uint32), 1, fp_s);
+    fseek(fp_s, 22, SEEK_SET);
+    fread(&info_header.height, sizeof(int32), 1, fp_s);
+    fseek(fp_s, 28, SEEK_SET);
+    fread(&info_header.bits_per_pixel, sizeof(uint16), 1, fp_s);
+
+/* messages of debugging header */
+#if DEBUG
+    printf( "**imread( '%s' ):\n", filename );
+    printf( "file_size: %d\n", file_header.file_size );
+    printf( "data_offset: %d\n", file_header.data_offset );
+    printf( "width: %d\n", info_header.width );
+    printf( "height: %d\n", info_header.height );
+    printf( "bits_per_pixel: %d\n", info_header.bits_per_pixel );
+#endif
+
+    /* 用現成的zeros來malloc就好 */
+    zeros( dest, abs( info_header.height ), info_header.width, info_header.bits_per_pixel / 8 );
+    fseek(fp_s, file_header.data_offset, SEEK_SET);
+    image_s = (unsigned char *)malloc(info_header.width * abs(info_header.height) * 3);
+    if (image_s == NULL) {
+        error("malloc images_s error\n");
+    }
+    fread(image_s, sizeof(unsigned char), (size_t)(long)info_header.width * abs( info_header.height ) * 3, fp_s);
+
+    for(layer=0;layer<3;layer++){
+        ine=layer;
+        for(row=0;row<dest->size1;row++){
+            dest->data[layer][row]=malloc( dest->size2 * sizeof( char ) );
+
+            for ( col = 0; col < dest->size2; col++ ) {
+				dest->data[ layer ][ row ][ col ] = image_s[ine];
+				ine=ine+3;
+            }
+        }
+
+    }
+
+    fclose(fp_s);
+    return;
+}
+
+void imwrite( char *filename, Matrix *dest ){
+
+
+}
+void color2Gray( Matrix *source ){ // dest is source itself
+
+}
+void gray2Color( Matrix *source ){ // dest is source itself
+
+
+}
+#if DEBUG
+int main(){
+    Matrix A;
+
+    imread(  "pics/paint_16.bmp", &A );
+    dump( &A, "A", BB, INT );
+
+    return 0;
+
+
+}
+
+#endif
