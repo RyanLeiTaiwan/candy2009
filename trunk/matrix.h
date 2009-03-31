28f10f7ed68a9633621ed82ca22b181f4545fbee
ryanlei750328 <ryanlei750328@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2009-03-31 20:20:58 +0000

    matrix.*: dump()必須指定rowBegin, rowEnd, colBegin, colEnd四個範圍 matrix.c, image.c: 修正test case的參數

--- trunk/matrix.h
+++ trunk/matrix.h
@@ -2,13 +2,12 @@
  ** Author: ryanlei
  ** Creation : 2009/03/21
  ** Modification: 2009/04/01
- ** Description: matrix data structure
+ ** Description: matrix data structure 
  **/
-
 #include "util.h"
 
 typedef struct Matrix {
-
+	
 	/* array of values, 先一律用float 3D array */
 	float ***data; /* [ layer ][ row ][ col ] */
 	/* dimensions */
@@ -19,29 +18,29 @@ typedef struct Matrix {
 } Matrix;
 
 /* matrix basics */
-typedef enum COLOR { RR, GG, BB, ALL } COLOR;
+typedef enum COLOR { RR, GG, BB, ALL } COLOR; 
 // 對於2D矩陣來說，RR和ALL意義是一樣的
 typedef enum TYPE { INT, DOUBLE } TYPE;
-void dump( Matrix *source, char *name, COLOR color, TYPE type );
+void dump( Matrix *source, char *name, COLOR color, int rowBegin, int rowEnd, int colBegin, int colEnd, TYPE type ); 
 // 顯示矩陣內容( 矩陣、名稱、第三維、INT或DOUBLE )
 void zeros( Matrix *source, int size1, int size2, int size3 ); // 產生全0的矩陣
 void ones( Matrix *source, int size1, int size2, int size3 ); // 產生全1的矩陣
 void eye( Matrix *source, int size ); // 產生2D Identity矩陣
-void RAND( Matrix *source, int size1, int size2, int size3, int lower, int upper );
+void RAND( Matrix *source, int size1, int size2, int size3, int lower, int upper ); 
 // 產生整數亂數[lower,upper]矩陣
 void freeMatrix( Matrix *source ); // 清除矩陣的記憶體空間
 
 /* scalar operations */
 void s_add( Matrix *source, float number ); // 元素加上一個純量
 void s_mul( Matrix *source, float number ); // 元素乘上一個純量（係數積）
-void s_pow( Matrix *source, float number ); // 元素的純量次方
+void s_pow( Matrix *source, float number ); // 元素的純量次方 
 
 /* matrix or element-wise operations */
-void m_add( Matrix *source1, Matrix *source2, Matrix *dest );
+void m_add( Matrix *source1, Matrix *source2, Matrix *dest ); 
 // 元素對應相加，sources不可和dest相同
-void e_mul( Matrix *source1, Matrix *source2, Matrix *dest );
+void e_mul( Matrix *source1, Matrix *source2, Matrix *dest ); 
 // 元素對應相乘，sources不可和dest相同
-void m_mul( Matrix *source1, Matrix *source2, COLOR color1, COLOR color2, Matrix *dest );
+void m_mul( Matrix *source1, Matrix *source2, COLOR color1, COLOR color2, Matrix *dest ); 
 // 2D矩陣相乘，sources皆可指定用哪些layer，sources不可和dest相同
 
 
