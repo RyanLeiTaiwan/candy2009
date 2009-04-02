a3a1768ec5f6a1d041a0ade7959a473f190f6210
medea425 <medea425@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2009-04-02 05:42:04 +0000

    imread : paint_16.bmp gray_15.bmp paint_61.bmp OK but paint_15.bmp partailly wrong!(the first column is wrong)

--- image.c
+++ image.c
@@ -8,7 +8,7 @@
 #include "image.h"
 #include "util.h"
 
-#define DEBUG 0
+#define DEBUG 1
 
 
 void imread( char *filename, Matrix *dest ){  // dest is not yet allocted
@@ -16,9 +16,11 @@ void imread( char *filename, Matrix *dest ){  // dest is not yet allocted
     BMP_file_header file_header;
     BMP_info_header info_header;
     uint8 *image_s = NULL; // temp one-dimension array to save
-    int col,row,layer,i;
+    int col,row,layer,ivalue,i;
     int mod4width,ine,hivalue,temp;
+    int pad_data_size;
     unsigned char headertemp[54];
+    int *record_ine=NULL;
     //dest=NULL;
     hivalue=1; //assuming the height value is positive
     mod4width=0; //asuming width mod 4=0
@@ -63,9 +65,13 @@ void imread( char *filename, Matrix *dest ){  // dest is not yet allocted
         error("the form is not correct.");
         }*/
   //  info_header.width=headertemp[18]+headertemp[19]*16+headertemp[20]*256+headertemp[21]*4096;
-    if(info_header.width%4!=0){
-        mod4width=4-info_header.width%4;
-        info_header.width=info_header.width+mod4width;
+    if(info_header.bits_per_pixel!=24 && info_header.bits_per_pixel!=8){
+        error("the bits per pixel is not 24 or 8.");
+        }
+    if(info_header.width%4!=0 ){
+        mod4width=4-((info_header.width*info_header.bits_per_pixel/8)%4);
+        printf(" mod4w=%d\n",mod4width);
+       // info_header.width=info_header.width+mod4width;
         }
     if(info_header.height<0){
         hivalue=0;
@@ -83,10 +89,8 @@ void imread( char *filename, Matrix *dest ){  // dest is not yet allocted
 
    // info_header.bits_per_pixel=headertemp[28]+headertemp[29]*16;////24
     //printf("xx=%d",info_header.bits_per_pixel);
-    if(info_header.bits_per_pixel!=24 && info_header.bits_per_pixel!=8){
-        error("the bits per pixel is not 24 or 8.");
-        }
 
+pad_data_size=info_header.height*mod4width+info_header.height*info_header.width;
 /* messages of debugging header */
 #if DEBUG
     printf( "**imread( '%s' ):\n", filename );
@@ -95,6 +99,8 @@ void imread( char *filename, Matrix *dest ){  // dest is not yet allocted
     printf( "width: %d\n", info_header.width );
     printf( "height: %d\n", info_header.height );
     printf( "bits_per_pixel: %d\n", info_header.bits_per_pixel );
+   // printf( "padded_data_size = %d\n", pad_data_size );
+
 #endif
 
     /* 用現成的zeros來malloc就好 */
@@ -103,16 +109,57 @@ void imread( char *filename, Matrix *dest ){  // dest is not yet allocted
     fseek(fp_s, file_header.data_offset, SEEK_SET);
 
     image_s = (unsigned char *)malloc(info_header.width * abs(info_header.height) * 3);
-
+    record_ine=(int*)malloc(info_header.height * mod4width );
     if (image_s == NULL) {
         error("malloc images_s error\n");
     }
 
     fread(image_s, sizeof(unsigned char), (size_t)(long)info_header.width * abs( info_header.height ) * 3, fp_s);
+    //ivalue=0;
+        /*if(mod4width==1 && info_header.bits_per_pixel==24){
+            ine=info_header.width*info_header.bits_per_pixel/8;
+
+            while(ine<3*info_header.width *info_header.height){
+                    record_ine[ivalue]=ine;
+                    ivalue++;
+                    printf("i=%d ",ine);
+                    ine=ine+info_header.width*info_header.bits_per_pixel/8+1;
+            }
+        }
+        else if(mod4width==2 && info_header.bits_per_pixel==24){
+             ine=info_header.width*info_header.bits_per_pixel/8;
+             while(ine<3*info_header.width *info_header.height){
+                    record_ine[ivalue]=ine;
+                    record_ine[ivalue+1]=ine+1;
+                    ivalue=ivalue+2;
+                    printf("i=%d ",ine);
+                    ine=ine+info_header.width*(info_header.bits_per_pixel/8)+2;
+             }
+        }
+        else if(mod4width==3 && info_header.bits_per_pixel==24){
+                ine=info_header.width*info_header.bits_per_pixel/8;
+                while(ine<3*info_header.width *info_header.height){
+
+                        record_ine[ivalue]=ine;
+                        record_ine[ivalue+1]=ine+1;
+                        record_ine[ivalue+2]=ine+2;
+                        printf("v:%d,%d,%d ",record_ine[ivalue],record_ine[ivalue+1],record_ine[ivalue+2]);
+                        ivalue=ivalue+3;
+                        printf("i=%d ",ine);
+                        //printf("value:%d ,%d ,%d",image_s[ine],image_s[ine+1],image_s[ine+2]);
+
+                        ine=ine+info_header.width*(info_header.bits_per_pixel/8)+3;
+
+                }
+
+        }*/
+
 
     if(info_header.bits_per_pixel==24){
+
         if(hivalue==0){  //height is negative
             for(layer=0;layer<info_header.bits_per_pixel/8 ;layer++){
+                i=info_header.width*3;
                 if(layer==0){
                     ine=2;
                     }
@@ -124,7 +171,39 @@ void imread( char *filename, Matrix *dest ){  // dest is not yet allocted
                 for(row=0;row<dest->size1;row++){
                     for ( col = 0; col < dest->size2; col++ ) {
                         dest->data[ layer][ row ][ col ] = image_s[ine];
+                       // printf("im_s=%d ",image_s[ine]);
                         ine=ine+3;
+                           if(mod4width==1){
+
+                                    if(ine==i || ine==i+1 || ine==i+2){
+                                       // printf("i+3:%d",ine);
+                                        ine++;
+                                        i=i+info_header.width*3+mod4width;
+                                    }
+
+                           }
+                            else if(mod4width==2){
+
+                                    if(ine==i || ine==i+1 || ine==ine+2){
+                                       // printf("i+2:%d",ine);
+                                        ine=ine+2;
+                                        i=i+info_header.width*3+mod4width;
+                                        }
+
+
+                                }
+                            else if(mod4width==3){
+
+                                    if(ine==i || ine==i+1 || ine==i+2){
+                                      //  printf("i+1:%d",ine);
+                                        ine=ine+3;
+                                        i=i+info_header.width*3+mod4width;
+                                    }
+
+
+
+                            }
+
                     }
                 }
 
@@ -132,6 +211,7 @@ void imread( char *filename, Matrix *dest ){  // dest is not yet allocted
         }
         else {  //height is positive
             for(layer=0;layer<info_header.bits_per_pixel/8 ;layer++){
+                i=info_header.width*3;
                 if(layer==0){
                     ine=2;
                     }
@@ -142,7 +222,45 @@ void imread( char *filename, Matrix *dest ){  // dest is not yet allocted
                 for(row=dest->size1-1;row>=0;row--){
                     for ( col = 0; col < dest->size2; col++ ) {
                         dest->data[ layer ][ row ][ col ] = image_s[ine];
+
+                       // printf("%d=%d  ",ine,image_s[ine]);
                         ine=ine+3;
+
+                            if(mod4width==1){
+
+                                   if(ine==i || ine==i+1 || ine==i+2){
+                                       // printf("i+3:%d",ine);
+                                        ine++;
+                                        i=i+info_header.width*3+mod4width;
+                                    }
+
+
+                            }
+                            else if(mod4width==2){
+                                  if(ine==i || ine==i+1 || ine==ine+2){
+                                       // printf("i+2:%d",ine);
+                                        ine=ine+2;
+                                        i=i+info_header.width*3+mod4width;
+                                        }
+                            }
+                            else if(mod4width==3){
+                                if(ine==i || ine==i+1 || ine==i+2){
+                                       // printf("X %d ",image_s[ine]);
+                                        ine=ine+3;
+                                        i=i+info_header.width*3+mod4width;
+                                    }
+                                   /* else if(ine==i+1){
+                                         printf("i+1=%d",i+1);
+                                        ine=ine+3;
+                                        i=i+info_header.width*3+mod4width;
+                                    }
+                                    else if(ine==i+2){
+                                         printf("i+2=%d",i+2);
+                                        ine=ine+3;
+                                        i=i+info_header.width*3+mod4width;
+                                    }*/
+                            }
+
                     }
                 }
             }
@@ -160,21 +278,67 @@ void imread( char *filename, Matrix *dest ){  // dest is not yet allocted
 
         layer=0;
         ine=0;
+        i=info_header.width;
         if(hivalue==0){  //height is negative
 
             for(row=0;row<dest->size1;row++){
                 for ( col = 0; col < dest->size2; col++ ) {
                     dest->data[ layer][ row ][ col ] = image_s[ine];
+                    //printf("di=%d ",image_s[ine]);
                     ine++;
+                    if(mod4width==1){
+                        if(ine==i) {
+                            //printf("==");
+                            ine++;
+                            i=i+info_header.width+mod4width;
+                         }
+                    }
+                    else if(mod4width==2){
+                            if(ine==i){
+                                ine=ine+2;
+                                i=i+info_header.width+mod4width;
+                            }
+                    }
+                    else if(mod4width==3){
+                            if(ine==i){
+                                ine=ine+3;
+                                i=i+info_header.width+mod4width;
+                            }
+
+                    }
                 }
             }
         }
+
+
         else {  //height is positive
             printf("p");
             for(row=dest->size1-1;row>=0;row--){
                 for ( col = 0; col < dest->size2; col++ ) {
                     dest->data[ layer ][ row ][ col ] = image_s[ine];
+                    //printf("di=%d ",image_s[ine]);
                     ine++;
+                   if(mod4width==1){
+                        if(ine==i) {
+                            //printf("==");
+                            ine++;
+                           // printf(" ine=%d ",ine);
+                            i=i+info_header.width+mod4width;
+                         }
+                    }
+                    else if(mod4width==2){
+                            if(ine==i){
+                                ine=ine+2;
+                                i=i+info_header.width+mod4width;
+                            }
+                    }
+                    else if(mod4width==3){
+                            if(ine==i){
+                                ine=ine+3;
+                                i=i+info_header.width+mod4width;
+                            }
+
+                    }
                 }
             }
 
@@ -204,8 +368,8 @@ int main(){
 
 	tic = clock();
 
-    imread(  "pics/gray_15.bmp", &A );
-    dump( &A, "A", RR, 0, A.size1-1, 0, A.size2-1, INT );
+    imread(  "pics/paint_61.bmp", &A );
+    dump( &A, "A", RR, 0, 15, 16, 31, INT );
     toc = clock();
 	/* 使用runningTime()來印計時結果 */
 	runningTime( tic, toc );
