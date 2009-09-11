f85ad63adcee582ccdd934a4a05f58f2470a31af
ryanlei750328 <ryanlei750328@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2009-09-11 13:17:12 +0000

    create empty files for the "fast car detection" project.

---
+++ trunk/source/car-train.c
@@ -0,0 +1,16 @@
+/** File: car-train.c
+ ** Author: Ryan Lei
+ ** Creation: 2009/09/11
+ ** Modification: XXXX/XX/XX
+ ** Description: The car-training program based on the paper:
+    Fast Human Detection Using a Novel Boosted Cascading Structure With Meta Stages, Chen and Chen, 2008.
+	Important techniques / concepts:
+	1. Real AdaBoost -> For now, use basic binary Adaboost
+	2. Cascaded classifiers -> For now, it is without meta stages (no confidence values)
+	3. Rectangle / Edge Orientation Histogram / Edge-Density features
+	4. Integral image / Integral histogram
+ **/
+
+void car_detect( int param1, int param2 ) {
+
+}
