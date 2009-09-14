36b5245eb09792a058535fea7212bf77f91643f5
ryanlei750328 <ryanlei750328@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2009-09-14 12:10:27 +0000

    make.sh: add car-train output car-train.*: basic blackbox, before the actual feature extraction.

--- make.sh
+++ make.sh
@@ -3,5 +3,6 @@
 # Description: put the "gcc" command in a shell script
 #   This script generates multiple execution files for large applications
            
-gcc -Wall -o hough source/util.c source/matrix.c source/image.c source/hough.c
-gcc -Wall -o hog source/util.c source/matrix.c source/image.c source/hog.c
+# gcc -Wall -o hough source/util.c source/matrix.c source/image.c source/hough.c
+# gcc -Wall -o hog source/util.c source/matrix.c source/image.c source/hog.c
+gcc -Wall -o car-train source/util.c source/matrix.c source/image.c source/car-extract.c source/car-train.c
