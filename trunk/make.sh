394442bee1a86fd2aeab36d7e74034d0aa7b2194
ryanlei750328 <ryanlei750328@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2009-11-22 10:41:28 +0000

    car-format.txt: Some updates. Forgot to commit previously make.sh: build for all executables hog.c: Resolve an issue from matrix.* change in r87:        emul(): Now assume dest has been allocated.        Just to make it work, still not obeying the "2009.08.03 suggestion" that 	     "Gaussian weight is to the whole block, not to each cell."

--- trunk/make.sh
+++ trunk/make.sh
@@ -3,8 +3,8 @@
 # Description: put the "gcc" command in a shell script
 #   This script generates multiple execution files for large applications
 cd source           
-# gcc -Wall -o ../hough util.c matrix.c image.c hough.c
-# gcc -Wall -o ../hog util.c matrix.c image.c hog.c
+gcc -Wall -o ../hough util.c matrix.c image.c hough.c
+gcc -Wall -o ../hog util.c matrix.c image.c hog.c
 gcc -Wall -o ../car-train util.c matrix.c image.c car-extract.c car-adaboost.c car-train.c
 gcc -Wall -o ../car-detect util.c matrix.c image.c car-extract.c car-detect.c
 cd ..
