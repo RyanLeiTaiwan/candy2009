7973f3f15f1d92e03a99561f68b25d0f8e488313
ryanlei750328 <ryanlei750328@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2009-12-25 15:29:11 +0000

    opencv hello world

---
+++ trunk/opencv/include/global.h
@@ -0,0 +1,13 @@
+/** File: global.h
+ ** Author: Ryan Lei
+ ** Creation: 2009/12/25
+ ** Update: XXXX/XX/XX
+ ** Description: Constants or Parameters used globally.
+ **/
+
+/* Constants */
+
+/* Parameters: may be edited */
+#define WINDOW_SIZE 128
+
+
