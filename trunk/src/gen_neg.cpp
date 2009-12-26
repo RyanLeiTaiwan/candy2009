9defd08266ceec4ae5cc27b6c482bc29ec6e932b
ryanlei750328 <ryanlei750328@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2009-12-26 11:06:18 +0000

    temp

---
+++ trunk/src/gen_neg.cpp
@@ -0,0 +1,18 @@
+/** File: gen_neg.cpp
+ ** Author: Ryan Lei
+ ** Creation: XXXX/XX/XX
+ ** Modification: XXXX/XX/XX
+ ** Description: Some program.
+ **/
+
+#include <iostream.h>
+#include <cxcore.h>
+#include <cv.h>
+
+void function( int param1, int param2 );
+
+int main() {
+	cout << "Hello\n";
+
+	return 0;
+}
---
+++ trunk/src/template.cpp
@@ -0,0 +1,17 @@
+/** File: template.cpp
+ ** Author: Ryan Lei
+ ** Creation: XXXX/XX/XX
+ ** Modification: XXXX/XX/XX
+ ** Description: Some program.
+ **/
+
+#include <iostream.h>
+#include <cxcore.h>
+#include <cv.h>
+
+void function( int param1, int param2 );
+
+int main() {
+
+	return 0;
+}
