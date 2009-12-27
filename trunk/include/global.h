fbeab3cebfa811712bf61c695412817ed92cdeaa
ryanlei750328 <ryanlei750328@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2009-12-27 11:08:15 +0000

    Create template and gen_neg projects

--- trunk/include/global.h
+++ trunk/include/global.h
@@ -10,5 +10,7 @@
 /* Parameters: may be edited */
 #define WINDOW_HEIGHT 96
 #define WINDOW_WIDTH 128 /* 3 : 4 */
+#define LARGE_HEIGHT 768
+#define LARGE_WIDTH 1024 /* 3 : 4, too */
 
 
--- trunk/src/gen_neg.cpp
+++ trunk/src/gen_neg.cpp
@@ -1,18 +1,40 @@
 /** File: gen_neg.cpp
  ** Author: Ryan Lei
- ** Creation: XXXX/XX/XX
- ** Modification: XXXX/XX/XX
- ** Description: Some program.
+ ** Creation: 2009/12/26
+ ** Modification: 2009/12/27
+ ** Description: Generate multiple negative images chopped
+ **              from large ones.
  **/
+
+/* Usage: gen_neg [INPUT_DIR] [OUTPUT_DIR] */ 
 
 #include <iostream.h>
 #include <cxcore.h>
 #include <cv.h>
+#include <dirent.h>
+#include "global.h"
 
-void function( int param1, int param2 );
-
-int main() {
-	cout << "Hello\n";
-
+int main( int argc, char *argv[] ) {
+	DIR *dir;
+	struct dirent *dp;
+	FILE *fin, *fout;
+	
+	if ( argc != 3 ) {
+		cout << "Usage: gen_neg [INPUT_DIR] [OUTPUT_DIR]" << endl;
+		exit( EXIT_FAILURE );
+	}
+	if ( !( dir = opendir( argv[ 1 ] ) ) ) {
+		cout << "gen_neg(): [INPUT_DIR] does not exist.\n";
+		exit( EXIT_FAILURE );
+	}
+	
+	cout << "gen_neg(): This program will take in a directory of large-sized images\n, " <<
+		"ASSUMING THE ASPECT RATIO is the same as" << LARGE_WIDTH << ":" << LARGE_HEIGHT << 
+	", then:\n 1. Resizing each image to exactly" << LARGE_WIDTH << "x"<< LARGE_HEIGHT << ".\n" <<
+	"2. Discard the 4 sides of the image by window size: " << WINDOW_WIDTH << "x" << WINDOW_HEIGHT << ".\n" <<
+	"3. Scan using the window, and write the new images to [OUTPUT_DIR]/FILENAME.jpg, where\n" <<
+	"   FILENAME is a digit counting from 1.\n";
+	
+	
 	return 0;
 }
