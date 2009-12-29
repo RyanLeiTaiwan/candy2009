be981fb175cf69fb2268fbf56ceb33032af58f10
ryanlei750328 <ryanlei750328@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2009-12-29 01:46:47 +0000

    car_train.cpp, car_adaboost.cpp, car_extract.*: File descriptions. car_train.cpp: User interface.

--- src/car_train.cpp
+++ src/car_train.cpp
@@ -11,21 +11,52 @@
       4. Integral image technique in computation
  **/
 
-/* Usage: car_train */
+/* Usage: car_train [POS_DIR] [NEG_DIR] [OUTPUT] */
 #include <iostream>
+#include <cstring>
+#include <dirent.h>
 #include <cxcore.h>
 #include <cv.h>
 #include "parameters.h"
 using namespace std;
 
-void function( int param1, int param2 );
-
-
-int main() {
+int main(int argc, char *argv[]) {
+	DIR *dir;
+	FILE *fout;
+	char slash = Unix ? '/' : '\\'; // It is '/' or '\' depending on OS
+	char POS_PATH_BASE[ MAX_PATH_LENGTH ];
+	char NEG_PATH_BASE[ MAX_PATH_LENGTH ];
 	
-	cout << "Hello, world2223322" << endl;
-	cout << WINDOW_WIDTH << ", " << WINDOW_HEIGHT << endl;
-	getchar();
+	if (argc != 4) {
+		cout << "Usage: car_train [POS_DIR] [NEG_DIR] [OUTPUT]\n";
+		exit(EXIT_FAILURE);
+	}
+	
+	if (!(dir = opendir(argv[ 1 ]))) {
+		cout << "car_train: [POS_DIR] does not exist.\n";
+		exit(EXIT_FAILURE);
+	}
+	/* Set POS_PATH_BASE to [POS_DIR] and append '/' or '\' */
+	strcpy(POS_PATH_BASE, argv[ 1 ]);	
+	sprintf(POS_PATH_BASE, "%s%c", POS_PATH_BASE, slash);
+	closedir(dir);
+	
+	if (!(dir = opendir(argv[ 2 ]))) {
+		cout << "car_train: [NEG_DIR] does not exist.\n";
+		exit(EXIT_FAILURE);
+	}
+	strcpy(NEG_PATH_BASE, argv[ 2 ]);	
+	sprintf(NEG_PATH_BASE, "%s%c", NEG_PATH_BASE, slash);	
+	closedir(dir);
+	
+	if (!(fout = fopen(argv[ 3 ], "w"))) {
+		cout << "car_train: [OUTPUT] does not exist.\n";
+		exit(EXIT_FAILURE);
+	}
 
+	
+	/** Command is correct **/
+	cout << "Command is correct.\n";
+	
 	return 0;
 }
