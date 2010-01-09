c610d27f7ef3896a665230c56fa977a7bf492d20
ryanlei750328 <ryanlei750328@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2010-01-09 13:42:38 +0000

    car_extract.*: Finished countBlocks(), extractAll(). Started extractImg(). car_train.cpp: Feature extraction and its user interface.

--- include/car_extract.h
+++ include/car_extract.h
@@ -1,12 +1,22 @@
 /** File: car_extract.h
  ** Author: Ryan Lei
  ** Creation: 2009/12/28
- ** Update: XXXX/XX/XX
+ ** Update: 2010/01/09
  ** Description: Header file of car feature extraction
  **/
 
 /* Internal parameters */
-#define FEATURE_COUNT 15
+/* feature-ralated constants */
+#define FEATURE_COUNT 15  
 #define REC_BEGIN 0
 #define EOH_BEGIN 5
 #define ED_BEGIN 14
+
+/* Function declrations */
+
+/* Feature extraction of the entire directory */
+void extractAll(char *PATH_BASE, CvMat **POOL, int *N, int *blockCount);
+/* Feature extraction of a single image */
+void extractImg(IplImage *img, CvMat *POOL);
+/* Count # of blocks in the image according to the internal parameters */
+int countBlocks(IplImage *img);
