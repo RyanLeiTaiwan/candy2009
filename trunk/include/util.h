98c65e73a07dab5b5d399de90894c20b69249ce3
ryanlei750328 <ryanlei750328@f3c005a8-16f7-11de-989c-57ac4e9a12f8>
2010-01-09 13:44:37 +0000

    add util.*: utility functions of countImages().

---
+++ trunk/include/util.h
@@ -0,0 +1,9 @@
+/** File: template.h
+ ** Author: Ryan Lei
+ ** Creation: 2010/01/09
+ ** Update: XXXX/XX/XX
+ ** Description: Header file for utility functions
+ **/
+
+
+int countImages(const char *PATH);
